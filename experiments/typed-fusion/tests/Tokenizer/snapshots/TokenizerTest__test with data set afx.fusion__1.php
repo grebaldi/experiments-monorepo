86b301111e69a9eafdba7c77c4313c46134339ce
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-02-01 19:49:26 +0100

    Add AFX example to Tokenizer fixtures

---
+++ experiments/typed-fusion/tests/Tokenizer/snapshots/TokenizerTest__test with data set afx.fusion__1.php
@@ -0,0 +1,53 @@
+<?php return '
+TYPE                           S_IDX S_ROW S_COL E_IDX E_ROW E_COL VALUE
+KEYWORD_PROTOTYPE              0     0     0     8     0     8     prototype
+BRACKET_ROUND_OPEN             9     0     9     9     0     9     (
+IDENTIFIER                     10    0     10    27    0     27    Vendor.Site:Teaser
+BRACKET_ROUND_CLOSE            28    0     28    28    0     28    )
+WHITESPACE                     29    0     29    29    0     29     
+OPERATOR_EXTENDS               30    0     30    30    0     30    <
+WHITESPACE                     31    0     31    31    0     31     
+KEYWORD_PROTOTYPE              32    0     32    40    0     40    prototype
+BRACKET_ROUND_OPEN             41    0     41    41    0     41    (
+IDENTIFIER                     42    0     42    62    0     62    Neos.Fusion:Component
+BRACKET_ROUND_CLOSE            63    0     63    63    0     63    )
+WHITESPACE                     64    0     64    64    0     64     
+BRACKET_CURLY_OPEN             65    0     65    65    0     65    {
+WHITESPACE                     66    0     66    70    1     3          
+IDENTIFIER                     71    1     4     75    1     8     image
+OPERATOR_ASSIGN_TYPE           76    1     9     76    1     9     :
+WHITESPACE                     77    1     10    77    1     10     
+IDENTIFIER                     78    1     11    99    1     32    Vendor.Site:Type.Image
+WHITESPACE                     100   1     33    104   2     3          
+IDENTIFIER                     105   2     4     109   2     8     title
+OPERATOR_ASSIGN_TYPE           110   2     9     110   2     9     :
+WHITESPACE                     111   2     10    111   2     10     
+IDENTIFIER                     112   2     11    117   2     16    string
+WHITESPACE                     118   2     17    118   2     17     
+OPERATOR_ASSIGN_VALUE          119   2     18    119   2     18    =
+WHITESPACE                     120   2     19    120   2     19     
+STRING_START                   121   2     20    121   2     20    \'
+STRING_END                     121   2     20    121   2     20    \'
+WHITESPACE                     123   2     22    127   3     3          
+IDENTIFIER                     128   3     4     138   3     14    description
+OPERATOR_ASSIGN_TYPE           139   3     15    139   3     15    :
+WHITESPACE                     140   3     16    140   3     16     
+IDENTIFIER                     141   3     17    146   3     22    string
+WHITESPACE                     147   3     23    147   3     23     
+OPERATOR_ASSIGN_VALUE          148   3     24    148   3     24    =
+WHITESPACE                     149   3     25    149   3     25     
+STRING_START                   150   3     26    150   3     26    \'
+STRING_END                     150   3     26    150   3     26    \'
+WHITESPACE                     152   3     28    157   5     3           
+IDENTIFIER                     158   5     4     165   5     11    renderer
+WHITESPACE                     166   5     12    166   5     12     
+OPERATOR_ASSIGN_VALUE          167   5     13    167   5     13    =
+WHITESPACE                     168   5     14    168   5     14     
+IDENTIFIER                     169   5     15    171   5     17    afx
+STRING_START                   172   5     18    172   5     18    `
+STRING_VALUE                   173   5     19    324   11    3              <div>             <img src={props.image.source}/>             <h2>{props.title}</h2>             <p>{props.description}</p>         </div>     
+STRING_END                     172   5     18    172   5     18    `
+WHITESPACE                     326   11    5     326   11    5      
+BRACKET_CURLY_CLOSE            327   12    0     327   12    0     }
+END_OF_FILE                    327   12    0     327   12    0     
+';
